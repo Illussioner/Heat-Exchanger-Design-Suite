f7cf8441c5995039ef20ff7c3967c8dcda4c7c04
Illussioner <illussionerstryder@gmail.com>
2026-07-12 18:13:45 +0530

    Added LMTD and heat transfer area calculations

--- include/HeatExchanger.h
+++ include/HeatExchanger.h
@@ -2,20 +2,31 @@
 #define HEATEXCHANGER_H
 
 #include "Fluid.h"
+enum class FlowType
+{
+  Counter,
+  Parallel
+};
 
 class HeatExchanger
 {
 private:
   Fluid hotFluid;
-
   Fluid coldFluid;
+  
+  double overallU;
 
+  FlowType flowtype;
 public:
   HeatExchanger();
 
   void inputData();
 
   void calculate();
+
+  double calculateLMTD() const;
+
+  double calculateArea(double heatDuty, double lmtd) const;
 };
 
 #endif
--- src/HeatExchanger.cpp
+++ src/HeatExchanger.cpp
@@ -12,14 +12,64 @@ void HeatExchanger::inputData()
 
   std::cout << "\n===== COLD FLUID =====\n";
   coldFluid.input();
+
+  std::cout << "\nOverall Heat Transfer Coefficient U (kW/m².K): ";
+  std::cin >> overallU;
+
+  int choice;
+
+  std::cout << "\nFlow Arrangement\n";
+  std::cout << "1. Counter Flow\n";
+  std::cout << "2. Parallel Flow\n";
+  std::cout << "Choice : ";
+
+  std::cin >> choice;
+
+  flowtype = (choice == 1) ? FlowType::Counter : FlowType::Parallel;
 }
 
 void HeatExchanger::calculate()
 {
-  double Qhot = hotFluid.heatDuty();
+  double heatDuty = std::abs(hotFluid.heatDuty());
+  double lmtd = calculateLMTD();
+  double area = calculateArea (heatDuty, lmtd);
+  std::cout << "\n=================================\n";
 
-  std::cout << "\n=========================\n";
-  std::cout << "Heat Duty = "
-            << std::abs(Qhot)
-            << " kW\n";
+  std::cout
+    << "Heat Duty :"
+    << heatDuty
+    <<"kW\n";
+
+  std::cout
+      << "LMTD : "
+      << lmtd
+      << " °C\n";
+
+  std::cout
+      << "Required Area : "
+      << area
+      << " m²\n";
 }
+
+double HeatExchanger::calculateLMTD() const{
+  double deltaT1;
+  double deltaT2;
+
+  if(flowtype == FlowType::Counter){
+    deltaT1 = hotFluid.inletTemperature - coldFluid.outletTemperature;
+
+    deltaT2 = hotFluid.outletTemperature - coldFluid.inletTemperature;
+  }
+
+  else{
+    deltaT1 = hotFluid.inletTemperature-coldFluid.inletTemperature;
+
+    deltaT2 = hotFluid.outletTemperature - coldFluid.outletTemperature;
+  }
+
+  return (deltaT1 - deltaT2) / std::log(deltaT1/ deltaT2);
+};
+
+double HeatExchanger::calculateArea(double heatDuty, double lmtd) const {
+  return heatDuty / (overallU * lmtd);
+};
